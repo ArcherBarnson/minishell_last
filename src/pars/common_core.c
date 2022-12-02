1b791fbcfee219d8de996083716eed919b53d430
Louis CALVIE <lcalvie@student.42.fr>
2022-12-02 16:20:51 +0100

    fix syntax error

--- src/pars/common_core.c
+++ src/pars/common_core.c
@@ -6,10 +6,10 @@
 /*   By: lcalvie <lcalvie@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 17:02:32 by mbourgeo          #+#    #+#             */
-/*   Updated: 2022/12/02 16:02:29 by lcalvie          ###   ########.fr       */
-/*   Updated: 2022/12/02 15:51:44 by lcalvie          ###   ########.fr       */
+/*   Updated: 2022/12/02 16:19:48 by lcalvie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 #include "minishell.h"
 
@@ -19,6 +19,7 @@ int	ft_read_prompt(t_shell *shell)
 	t_pars		pars;
 	char		*temp;
 
+	temp = NULL;
 	ft_init_core(&lex, &pars, shell);
 	if (!pars.there_hdoc)
 		temp = lex.user_input;
@@ -27,6 +28,14 @@ int	ft_read_prompt(t_shell *shell)
 	pars.r = ft_all_parsing_steps(&lex, &pars, shell);
 	if (pars.r == 45)
 		return (ft_error_ctrl_c(shell, &pars, &lex));
+	else if (pars.r == 44)
+	{
+		ft_lstclear(&pars.cmd, del);
+		ft_lstclear(&shell->cmd, del);
+		if (pars.command)
+			ft_free_commandtokenlol(pars.command);
+		ft_pars_freeall(&pars);
+	}
 	if (!pars.there_hdoc)
 	{
 		free(temp);
@@ -103,11 +112,17 @@ int	ft_init_core(t_lex *lex, t_pars *pars, t_shell *shell)
 int	ft_all_parsing_steps(t_lex *lex, t_pars *pars, t_shell *shell)
 {
 	int	r;
+	int r0;
 
 	if (ft_around_lexer(lex) || ft_debug_content(lex, pars, "lex"))
 		return (ft_error_return(lex, pars, shell));
-	if (ft_around_parser(lex, pars)  || ft_debug_content(lex, pars, "pars"))
+	r0 = ft_around_parser(lex, pars);
+	if (r0 || ft_debug_content(lex, pars, "pars"))
+	{
+		if (r0 == 45)
+			return (44);
 		return (ft_error_return(lex, pars, shell));
+	}
 	pars->there_hdoc = 0;
 	pars->lock_there_hdoc = 0;
 	if (ft_expander(pars)  || ft_debug_content(lex, pars, "exp"))
